c154f334b7e935de0b64c8f4c2cae082afe7c90d
constant-student <rudnev.danilka@mail.ru>
2023-11-01 15:18:38 +0700

    01.11.23

---
+++ read_from_file/read_file.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int N_string(FILE* text);
+
+double** read(FILE* text);
---
+++ read_from_file/read_fule.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int N_string(FILE* input)
+{
+    
+}
+
+
+int main()
+{
+    char a;
+    FILE* text=fopen("text.txt","r");
+    while(1)
+        a=fgetc(text);
+        
+}
